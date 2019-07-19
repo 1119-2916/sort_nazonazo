f089965e16fa73024922b5ea46fbd4d783136c02
T.Ikeda <ti11192916@gmail.com>
2019-07-19 09:30:44 +0900

    [fix] リファクタリング 処理を全て関数に切り出し

--- sort_nazonazo_bot.py
+++ sort_nazonazo_bot.py
@@ -67,7 +67,7 @@ def hard_reset():
     global answer
     answer = ''
 
-# コマンドを知るコマンド
+# コマンドを知るコマンドのパース
 def cmd_list(cmd):
     if cmd.find('-cmd') != -1:
         print("cmd command is called")
@@ -75,7 +75,11 @@ def cmd_list(cmd):
     else:
         return False
 
-# テスト用echoコマンド
+# コマンドを知るコマンド
+async def run_list(message):
+    await message.channel.send('出題: -prob\n問題数を見る: -size\n問題のヒントを見る: -hint NUM\n問題を諦める: -giveup\n困った時は: -reset')
+
+# テスト用echoコマンドのパース
 def cmd_echo(cmd):
     if cmd.find('-echo') != -1:
         print("echo command is called")
@@ -83,7 +87,11 @@ def cmd_echo(cmd):
     else:
         return False
     
-# 問題数を知るコマンド
+# テスト用echo関数
+async def run_echo(message):
+    await message.channel.send(message.content)
+
+# 問題数を知るコマンドのパース
 def cmd_problem_size(cmd):
     if cmd.find('-size') != -1:
         print("size command is called")
@@ -91,7 +99,12 @@ def cmd_problem_size(cmd):
     else:
         return False
 
-# 問題を出題するコマンド
+# 問題数を知るコマンド
+async def run_problem_size(message):
+    global dictionary
+    await message.channel.send('全部で' + str(len(dictionary)) + '問あります')
+
+# 問題を出題するコマンドのパース
 def cmd_question(cmd):
     if cmd.find('-prob') != -1:
         print("prob command is called")
@@ -99,7 +112,23 @@ def cmd_question(cmd):
     else:
         return False
 
-# 問題を諦めるコマンド
+# 問題を出題するコマンド
+async def run_question(message):
+    global question_solving
+    global dictionary
+    global problem
+    global answer
+    if question_solving:
+        await message.channel.send('前回の出題が解かれていません\n問題: ' + problem)
+    else:
+        question_solving = True
+        tmp = get_problem(dictionary)
+        print(tmp)
+        answer = tmp[0]
+        problem = tmp[1]
+        await message.channel.send('ソートなぞなぞ ソート前の文字列な〜んだ？\n' + problem)
+
+# 問題を諦めるコマンドのパース
 def cmd_giveup(cmd):
     if cmd.find('-giveup') != -1:
         print("giveup command is called")
@@ -107,7 +136,22 @@ def cmd_giveup(cmd):
     else:
         return False
 
-# 問題のヒントを出すコマンド
+# 問題を諦めるコマンド
+async def run_giveup(message):
+    global question_solving
+    global problem
+    global answer
+    if question_solving:
+        response = '正解は\"' + answer + '\"でした...'
+        await message.channel.send(response)
+        question_solving = False
+        problem = ''
+        answer = ''
+    else:
+        response = '現在問題は出されていません'
+        await message.channel.send(response)
+
+# 問題のヒントを出すコマンドのパース
 def cmd_hint(cmd):
     if cmd.find('-hint') != -1:
         print("hint command is called")
@@ -115,7 +159,29 @@ def cmd_hint(cmd):
     else:
         return False
 
-# bot の状態を初期化するコマンド
+# 問題のヒントを出すコマンド
+async def run_hint(message):
+    global question_solving
+    global answer
+    if not question_solving:
+        response = '現在問題は出されていません'
+        await message.channel.send(response)
+        return
+    hint_length = 1
+    for cmds in message.content.split(' '):
+        if cmds.isdecimal():
+            hint_length = int(cmds)
+            break
+    #await message.channel.send(str(hint_length) + '文字ヒント:', end='');
+    await message.channel.send(str(hint_length) + '文字ヒント:')
+    if hint_length >= len(answer):
+        await message.channel.send('答えが知りたい場合は -giveup を使用して下さい。')
+        return
+    if question_solving:
+        response = '答えは先頭の' + str(hint_length) + '文字は\"' + answer[0:hint_length] + '\"です'
+        await message.channel.send(response)
+
+# bot の状態を初期化するコマンドのパース
 def cmd_reset(cmd):
     if cmd.find('-reset') != -1:
         print("reset command is called")
@@ -123,7 +189,12 @@ def cmd_reset(cmd):
     else:
         return False
 
-# bot を終了するコマンド
+# bot の状態を初期化するコマンド
+async def run_reset(message):
+    hard_reset()
+    await message.channel.send('botの状態を初期化しました。')
+
+# bot を終了するコマンドのパース
 def cmd_quit(cmd):
     if cmd.find('-bye') != -1:
         print("bye command is called")
@@ -131,9 +202,10 @@ def cmd_quit(cmd):
     else:
         return False
 
-# テスト用echo関数
-async def run_echo(message):
-    await message.channel.send(message.content)
+# bot を終了するコマンド
+async def run_quit(message):
+    await message.channel.send('botを終了しました。')
+    sys.exit()
 
 @client.event
 async def on_ready():
@@ -142,7 +214,6 @@ async def on_ready():
 @client.event
 async def on_message(message):
     global question_solving
-    global dictionary
     global problem
     global answer
     if message.author == client.user:
@@ -163,48 +234,24 @@ async def on_message(message):
         await run_echo(message)
 
     if cmd_list(message.content):
-        await message.channel.send('出題: -prob\n問題数を見る: -size\n問題のヒントを見る: -hint\n問題を諦める: -giveup\n困った時は: -reset')
+        await run_list(message)
 
     if cmd_problem_size(message.content):
-        await message.channel.send('全部で' + str(len(dictionary)) + '問あります')
+        await run_problem_size(message)
 
     if cmd_reset(message.content):
-        hard_reset()
-        await message.channel.send('botの状態を初期化します')
+        await run_reset(message)
 
     if cmd_hint(message.content):
-        if question_solving:
-            response = '先頭の文字は\"' + answer[0] + '\"です'
-            await message.channel.send(response)
-        else:
-            response = '現在問題は出されていません'
-            await message.channel.send(response)
+        await run_hint(message)
 
     if cmd_giveup(message.content):
-        if question_solving:
-            response = '正解は\"' + answer + '\"でした...'
-            await message.channel.send(response)
-            question_solving = False
-            problem = ''
-            answer = ''
-        else:
-            response = '現在問題は出されていません'
-            await message.channel.send(response)
+        await run_giveup(message)
 
     if cmd_question(message.content):
-        if question_solving:
-            await message.channel.send('前回の出題が解かれていません\n問題: ' + problem)
-        else:
-            question_solving = True
-            tmp = get_problem(dictionary)
-            print(tmp)
-            answer = tmp[0]
-            problem = tmp[1]
-            print(type(problem))
-            await message.channel.send('ソートなぞなぞ ソート前の文字列な〜んだ？\n' + problem)
+        await run_question(message)
 
     if cmd_quit(message.content):
-        await message.channel.send('botを終了しました。')
-        sys.exit()
+        await run_quit(message)
 
 client.run(token)
