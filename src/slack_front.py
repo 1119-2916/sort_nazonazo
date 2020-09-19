1324e77e7390dd0d9cda5ffbf5499a8adf7e607a
1119-2916 <ti11192916@gmail.com>
2020-09-19 10:34:54 +0900

    update : echo

--- src/slack_front.py
+++ src/slack_front.py
@@ -1,10 +1,24 @@
-from flask import abort, Flask, jsonify, request
-from slackeventsapi import SlackEventAdapter
 import os
+import sys
+
+from flask import Flask
+from slack import WebClient
+from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
 
 slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], "/slack/events", app)
+
+slack_bot_token = os.environ["SLACK_OAUTH_TOKEN"]
+slack_client = WebClient(slack_bot_token)
+
+
 @slack_events_adapter.on("message")
-def hookSlackEvents(event_data):
-        app.logger.debug(event_data)
+def handle_message_greeting(event_data):
+    print("debug:handled function: {}".format(sys._getframe().f_code.co_name))
+    print("debug:eventdata:{}".format(event_data))
+    message = event_data["event"]
+    if message.get("subtype") is None and message.get("bot_id") is None:
+        channel = message["channel"]
+        res_message = message.get("text") + ":dolphin:"
+        slack_client.chat_postMessage(channel=channel, text=res_message)
